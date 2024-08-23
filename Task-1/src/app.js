c4d249fefe4035d8ddac88059cc3dd40595ef982
sumit-thakur-08 <sumit.thakur.official.2004@gmail.com>
2024-08-24 01:22:34 +0530

    CRUD application for event handling using Node.js and MongoDB

--- Task-1/src/app.js
+++ Task-1/src/app.js
@@ -3,19 +3,20 @@ import cors from "cors";
 
 const app = express();
 
+// use cors middleware
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "16kb" })); // set limti to accept json data
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));  // set limit to accept url data
 app.use(express.static("./public"));
 
 
-import eventRouter from "./routes/event.routes.js";
+import eventRouter from "./routes/event.routes.js";    
 
-app.use("/api/v3/app", eventRouter);
+app.use("/api/v3/app", eventRouter);    // configure route
 
 
 export { app }
